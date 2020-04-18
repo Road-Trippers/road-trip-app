e3b57e64bd46a9d158b655d3e0987ae12ddabd4b
ggank <13955056+kaygrog@users.noreply.github.com>
2020-04-18 11:15:55 -0500

    Add local storage implementation

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,133 +1,159 @@
 // Create object to hold user input of latitude/longitude coordinates
 var input = {
-	starting: { latitude: 0, longitude: 0 },
-	destination: { latitude: 0, longitude: 0 },
+  starting: { latitude: 0, longitude: 0 },
+  destination: { latitude: 0, longitude: 0 },
 };
+
+var playlist = [];
+var playlistDiv = document.getElementById("playlist-div");
+var tripDurationHTML = document.getElementById("trip-duration");
+var playlistDurationHTML = document.getElementById("playlist-duration");
+
+debugger;
+// Grab any existing data from local storage
+if (localStorage.getItem("playlist") !== null) {
+  var tripDuration = JSON.parse(localStorage.getItem("trip-duration"));
+  var playlistDuration = JSON.parse(localStorage.getItem("playlist-duration"));
+  playlist = JSON.parse(localStorage.getItem("playlist"));
+  showData(tripDuration, playlistDuration);
+} else {
+  playlist = [];
+}
+
+function showData(tripDuration, playlistDuration) {
+  playlistDiv.textContent = "";
+  tripDurationHTML.textContent = "";
+  playlistDurationHTML.textContent = "";
+
+  // Put durations into HTML
+  tripDurationHTML.textContent =
+    "Trip Duration: " + (tripDuration / 60).toFixed(2) + " minutes";
+  playlistDurationHTML.textContent =
+    "Playlist Duration: " + (playlistDuration / 60).toFixed(2) + " minutes";
+
+  // Put playlist into HTML
+  for (let index = 0; index < playlist.length; index++) {
+    const element = playlist[index];
+    var songDiv = document.createElement("p");
+    songDiv.textContent = element.title;
+    playlistDiv.appendChild(songDiv);
+  }
+}
 
 // When Get Playlist buton is clicked, run the following function
 document
-	.getElementById("submit-button")
-	.addEventListener("click", async function (event) {
-		// Prevent page from refreshing upon button click
-		event.preventDefault();
+  .getElementById("submit-button")
+  .addEventListener("click", async function (event) {
+    // Prevent page from refreshing upon button click
+    event.preventDefault();
 
-		// Get user input from fields
-		var startingPoint = document.getElementById("startingPoint").value;
-		var destination = document.getElementById("endPoint").value;
+    // Get user input from fields
+    var startingPoint = document.getElementById("startingPoint").value;
+    var destination = document.getElementById("endPoint").value;
 
-		// Store user input coordinates as an array split at comma [latitude, longitude]
-		startingPoint = startingPoint.split(",");
-		destination = destination.split(",");
+    // Store user input coordinates as an array split at comma [latitude, longitude]
+    startingPoint = startingPoint.split(",");
+    destination = destination.split(",");
 
-		// Store user input that is stored in array in the object created above
-		input = {
-			starting: { latitude: startingPoint[0], longitude: startingPoint[1] },
-			destination: { latitude: destination[0], longitude: destination[1] },
-		};
+    // Store user input that is stored in array in the object created above
+    input = {
+      starting: { latitude: startingPoint[0], longitude: startingPoint[1] },
+      destination: { latitude: destination[0], longitude: destination[1] },
+    };
 
-		// Create Mapbox query URL based on user input
-		var mapBoxQueryURL =
-			"https://api.mapbox.com/directions/v5/mapbox/driving/" +
-			input.starting.longitude +
-			"%2C" +
-			input.starting.latitude +
-			"%3B" +
-			input.destination.longitude +
-			"%2C" +
-			input.destination.latitude +
-			"?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1Ijoia2F5Z3JvZyIsImEiOiJjazh2dnBueXgwMnltM3ByejY2Y3hlM25kIn0.jyN4BskKiVdTLxWbxcPWsw";
+    // Create Mapbox query URL based on user input
+    var mapBoxQueryURL =
+      "https://api.mapbox.com/directions/v5/mapbox/driving/" +
+      input.starting.longitude +
+      "%2C" +
+      input.starting.latitude +
+      "%3B" +
+      input.destination.longitude +
+      "%2C" +
+      input.destination.latitude +
+      "?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1Ijoia2F5Z3JvZyIsImEiOiJjazh2dnBueXgwMnltM3ByejY2Y3hlM25kIn0.jyN4BskKiVdTLxWbxcPWsw";
 
-		// Call function to make Mapbox API call; await until API call is done and store response in mapResponse variable
-		var mapResponse = await apiCall(mapBoxQueryURL);
+    // Call function to make Mapbox API call; await until API call is done and store response in mapResponse variable
+    var mapResponse = await apiCall(mapBoxQueryURL);
 
-		// Store trip duration from first route from API call response
-		var tripDuration = mapResponse.routes[0].duration;
+    // Store trip duration from first route from API call response
+    var tripDuration = mapResponse.routes[0].duration;
 
-		// Convert duration to minutes
-		var minutes = tripDuration / 60;
+    // Convert duration to minutes
+    var minutes = tripDuration / 60;
 
-		// Call function to make Deezer API call; await until API call is done and store response in songs variable
-		var playlistDiv = document.getElementById("playlist-div");
-		var playlist = [];
-		var playlistData = await createPlaylist(tripDuration, 0);
-		var playlist = playlistData.playlist;
-		var playlistDuration = playlistData.duration;
+    playlist.length = 0;
 
-		//playlist.forEach((e) => (playlistDuration += Number(e.duration)));
-		for (let index = 0; index < playlist.length; index++) {
-			const element = playlist[index];
-			var songDiv = document.createElement("p");
-			songDiv.textContent = element.title;
-			playlistDiv.appendChild(songDiv);
-		}
+    // Call function to make Deezer API call; await until API call is done and store response in songs variable
+    var playlistDuration = await createPlaylist(tripDuration, 0);
 
-		// Put trip duration into HTML
-		var tripDurationHTML = document.getElementById("trip-duration");
-		tripDurationHTML.textContent =
-			"Trip Duration: " + minutes.toFixed(2) + " minutes";
-		var playlistDurationDiv = document.getElementById("playlist-duration");
-		playlistDurationDiv.textContent =
-			"Playlist Duration: " + (playlistDuration / 60).toFixed(2) + " minutes";
-	});
+    showData(tripDuration, playlistDuration);
+
+    // Store playlist in local storage
+    localStorage.clear();
+    localStorage.setItem("playlist", JSON.stringify(playlist));
+    localStorage.setItem("trip-duration", JSON.stringify(tripDuration));
+    localStorage.setItem("playlist-duration", JSON.stringify(playlistDuration));
+  });
 
 async function getSongs(URL) {
-	// Create query URL for Deezer API call
-	var deezerQueryURL = URL;
+  // Create query URL for Deezer API call
+  var deezerQueryURL = URL;
 
-	// Make Deezer API call; await until API call is done and store response
-	var deezerResponse = await apiCall(deezerQueryURL);
+  // Make Deezer API call; await until API call is done and store response
+  var deezerResponse = await apiCall(deezerQueryURL);
 
-	// Create array to hold song data
-	var songs = [];
+  // Create array to hold song data
+  var songs = [];
 
-	// Loop through API response array and add applicable song data to new songs array
-	for (var i = 0; i < deezerResponse.data.length; i++) {
-		var e = deezerResponse.data[i];
-		songs.push({ title: e.title, artist: e.artist.name, duration: e.duration });
-	}
-	return { songs: songs, next: deezerResponse.next };
+  // Loop through API response array and add applicable song data to new songs array
+  for (var i = 0; i < deezerResponse.data.length; i++) {
+    var e = deezerResponse.data[i];
+    songs.push({ title: e.title, artist: e.artist.name, duration: e.duration });
+  }
+  return songs;
 }
 
-async function createPlaylist(tripDuration, runningTotal) {
-	var songs = await getSongs(getDeezerUrl(playlist.length));
+async function createPlaylist(tripDuration, playlistDuration) {
+  var songs = await getSongs(createDeezerUrl(playlist.length));
 
-	for (let i = 0; i < songs.songs.length && runningTotal < tripDuration; i++) {
-		playlist.push(songs.songs[i]);
-		runningTotal += songs.songs[i].duration;
-	}
-	if (runningTotal < tripDuration) {
-		await createPlaylist(tripDuration, runningTotal);
-	}
+  for (let i = 0; i < songs.length && playlistDuration < tripDuration; i++) {
+    playlist.push(songs[i]);
+    playlistDuration += songs[i].duration;
+  }
+  if (playlistDuration < tripDuration) {
+    return createPlaylist(tripDuration, playlistDuration);
+  }
 
-	return runningTotal;
+  return playlistDuration;
 }
 function createDeezerUrl(index) {
-	return (
-		"https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem&index=" + index
-	);
+  return (
+    "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem&index=" + index
+  );
 }
 // API call function
 function apiCall(queryURL) {
-	return new Promise((resolve, reject) => {
-		const xhr = new XMLHttpRequest();
-		xhr.open("GET", queryURL);
-		if (queryURL.includes("deezer")) {
-			xhr.setRequestHeader(
-				"x-rapidapi-host",
-				"deezerdevs-deezer.p.rapidapi.com"
-			);
-			xhr.setRequestHeader(
-				"x-rapidapi-key",
-				"ec3c5ea0ebmsh0160b4b2056a0a1p1c3058jsn53757ea9b4b5"
-			);
-		}
-		xhr.onload = function () {
-			if (xhr.status === 200) {
-				resolve(JSON.parse(xhr.responseText));
-			} else {
-				reject("Request failed.  Returned status of " + xhr.status);
-			}
-		};
-		xhr.send();
-	});
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", queryURL);
+    if (queryURL.includes("deezer")) {
+      xhr.setRequestHeader(
+        "x-rapidapi-host",
+        "deezerdevs-deezer.p.rapidapi.com"
+      );
+      xhr.setRequestHeader(
+        "x-rapidapi-key",
+        "ec3c5ea0ebmsh0160b4b2056a0a1p1c3058jsn53757ea9b4b5"
+      );
+    }
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        reject("Request failed.  Returned status of " + xhr.status);
+      }
+    };
+    xhr.send();
+  });
 }
