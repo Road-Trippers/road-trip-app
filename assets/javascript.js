01255fd9fc051cfd5aa7b523ad8e6cd383d0097d
ggank <13955056+kaygrog@users.noreply.github.com>
2020-04-18 14:08:13 -0500

    Incorporate firebase storage of recent artists

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,184 +4,224 @@ var input = {};
 var playlist = [];
 var artist;
 var urlIndex = 0;
+var recentArtists = [];
 var playlistDiv = document.getElementById("playlist-div");
 var tripDurationHTML = document.getElementById("trip-duration");
 var playlistDurationHTML = document.getElementById("playlist-duration");
+
 //to connect to library to convertaddress to lat/long
 var placesStart = places({
-	appId: "plRUJ22SXG0U",
-	apiKey: "7621bbb9b57f4f8ddb04604ff8d24a00",
-	container: document.querySelector("#startingPoint"),
+  appId: "plRUJ22SXG0U",
+  apiKey: "7621bbb9b57f4f8ddb04604ff8d24a00",
+  container: document.querySelector("#startingPoint"),
 });
 
 var placesEnd = places({
-	appId: "plRUJ22SXG0U",
-	apiKey: "7621bbb9b57f4f8ddb04604ff8d24a00",
-	container: document.querySelector("#endPoint"),
+  appId: "plRUJ22SXG0U",
+  apiKey: "7621bbb9b57f4f8ddb04604ff8d24a00",
+  container: document.querySelector("#endPoint"),
 });
 
 //on event - clicking 'get playlist' button, grab lat/long from variables where stored
 placesStart.on("clear", function (suggestion) {
-	input.starting = "";
+  input.starting = "";
 });
 
 placesEnd.on("clear", function (suggestion) {
-	input.destination = "";
+  input.destination = "";
 });
 
 placesStart.on("change", function (suggestion) {
-	input.starting = suggestion.suggestion.latlng;
+  input.starting = suggestion.suggestion.latlng;
 });
 
 placesEnd.on("change", function (suggestion) {
-	input.destination = suggestion.suggestion.latlng;
+  input.destination = suggestion.suggestion.latlng;
+});
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA2Kgq-Zt9JprHavAmXC6lkG_gQnvvBg-c",
+  authDomain: "coding-bootcamp-c2d63.firebaseapp.com",
+  databaseURL: "https://coding-bootcamp-c2d63.firebaseio.com",
+  projectId: "coding-bootcamp-c2d63",
+  storageBucket: "coding-bootcamp-c2d63.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+// At the initial load and subsequent value changes, get a snapshot of the stored data.
+database.ref().once("value", function (snapshot) {
+  console.log(snapshot.val());
+
+  var recentArtistsHTML = document.getElementById("recent-artists");
+  recentArtists = snapshot.val().recentArtists;
+  recentArtists.length = 5;
+
+  for (var i = 0; i < recentArtists.length; i++) {
+    if (recentArtists[i]) {
+      recentArtistsHTML.textContent += recentArtists[i];
+    }
+  }
 });
 
 // Grab any existing data from local storage
 if (localStorage.getItem("playlist") !== null) {
-	var tripDuration = JSON.parse(localStorage.getItem("trip-duration"));
-	var playlistDuration = JSON.parse(localStorage.getItem("playlist-duration"));
-	playlist = JSON.parse(localStorage.getItem("playlist"));
-	showData(tripDuration, playlistDuration);
+  var tripDuration = JSON.parse(localStorage.getItem("trip-duration"));
+  var playlistDuration = JSON.parse(localStorage.getItem("playlist-duration"));
+  playlist = JSON.parse(localStorage.getItem("playlist"));
+  showData(tripDuration, playlistDuration);
 } else {
-	playlist = [];
+  playlist = [];
 }
 
 function showData(tripDuration, playlistDuration) {
-	playlistDiv.textContent = "";
-	tripDurationHTML.textContent = "";
-	playlistDurationHTML.textContent = "";
+  playlistDiv.textContent = "";
+  tripDurationHTML.textContent = "";
+  playlistDurationHTML.textContent = "";
 
-	// Put durations into HTML
-	tripDurationHTML.textContent =
-		"Trip Duration: " + (tripDuration / 60).toFixed(2) + " minutes";
-	playlistDurationHTML.textContent =
-		"Playlist Duration: " + (playlistDuration / 60).toFixed(2) + " minutes";
+  // Put durations into HTML
+  tripDurationHTML.textContent =
+    "Trip Duration: " + (tripDuration / 60).toFixed(2) + " minutes";
+  playlistDurationHTML.textContent =
+    "Playlist Duration: " + (playlistDuration / 60).toFixed(2) + " minutes";
 
-	// Put playlist into HTML
-	for (let index = 0; index < playlist.length; index++) {
-		const element = playlist[index];
-		var songDiv = document.createElement("p");
-		songDiv.textContent = element.title;
-		playlistDiv.appendChild(songDiv);
-	}
+  // Put playlist into HTML
+  for (let index = 0; index < playlist.length; index++) {
+    const element = playlist[index];
+    var songDiv = document.createElement("p");
+    songDiv.textContent = element.title;
+    playlistDiv.appendChild(songDiv);
+  }
 }
 
 // When Get Playlist button is clicked, run the following function
 document
-	.getElementById("submit-button")
-	.addEventListener("click", async function (event) {
-		// Prevent page from refreshing upon button click
-		event.preventDefault();
+  .getElementById("submit-button")
+  .addEventListener("click", async function (event) {
+    // Prevent page from refreshing upon button click
+    event.preventDefault();
 
-		//if starting or destination fields are empty, exit the function
-		if (!input.starting || !input.destination) {
-			return;
-		}
+    //if starting or destination fields are empty, exit the function
+    if (!input.starting || !input.destination) {
+      return;
+    }
 
-		artist = document.getElementById("artist").value.split(",");
-		// Create Mapbox query URL based on user input
-		var mapBoxQueryURL =
-			"https://api.mapbox.com/directions/v5/mapbox/driving/" +
-			input.starting.lng +
-			"%2C" +
-			input.starting.lat +
-			"%3B" +
-			input.destination.lng +
-			"%2C" +
-			input.destination.lat +
-			"?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1Ijoia2F5Z3JvZyIsImEiOiJjazh2dnBueXgwMnltM3ByejY2Y3hlM25kIn0.jyN4BskKiVdTLxWbxcPWsw";
+    artist = document.getElementById("artist").value.split(",");
+    artist = artist.map((e) => e.trim());
+    recentArtists.unshift(...artist);
+    console.log(recentArtists);
 
-		// Call function to make Mapbox API call; await until API call is done and store response in mapResponse variable
-		var mapResponse = await apiCall(mapBoxQueryURL);
+    database.ref().set({
+      recentArtists: recentArtists,
+    });
 
-		// Store trip duration from first route from API call response
-		var tripDuration = mapResponse.routes[0].duration;
+    // Create Mapbox query URL based on user input
+    var mapBoxQueryURL =
+      "https://api.mapbox.com/directions/v5/mapbox/driving/" +
+      input.starting.lng +
+      "%2C" +
+      input.starting.lat +
+      "%3B" +
+      input.destination.lng +
+      "%2C" +
+      input.destination.lat +
+      "?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1Ijoia2F5Z3JvZyIsImEiOiJjazh2dnBueXgwMnltM3ByejY2Y3hlM25kIn0.jyN4BskKiVdTLxWbxcPWsw";
 
-		playlist.length = 0;
+    // Call function to make Mapbox API call; await until API call is done and store response in mapResponse variable
+    var mapResponse = await apiCall(mapBoxQueryURL);
 
-		// Call function to make Deezer API call; await until API call is done and store response in songs variable
-		var playlistDuration = await createPlaylist(tripDuration, 0);
+    // Store trip duration from first route from API call response
+    var tripDuration = mapResponse.routes[0].duration;
 
-		showData(tripDuration, playlistDuration);
+    playlist.length = 0;
 
-		// Store playlist in local storage
-		localStorage.clear();
-		localStorage.setItem("playlist", JSON.stringify(playlist));
-		localStorage.setItem("trip-duration", JSON.stringify(tripDuration));
-		localStorage.setItem("playlist-duration", JSON.stringify(playlistDuration));
-	});
+    // Call function to make Deezer API call; await until API call is done and store response in songs variable
+    var playlistDuration = await createPlaylist(tripDuration, 0);
+
+    showData(tripDuration, playlistDuration);
+
+    // Store playlist in local storage
+    localStorage.clear();
+    localStorage.setItem("playlist", JSON.stringify(playlist));
+    localStorage.setItem("trip-duration", JSON.stringify(tripDuration));
+    localStorage.setItem("playlist-duration", JSON.stringify(playlistDuration));
+  });
 
 async function getSongs() {
-	// Create array to hold song data
+  // Create array to hold song data
 
-	var songs = [];
-	for (var i = 0; i < artist.length; i++) {
-		// Create query URL for Deezer API call
-		// Make Deezer API call; await until API call is done and store response
-		var deezerResponse = await apiCall(
-			createDeezerUrl(urlIndex * 25, artist[i])
-		);
+  var songs = [];
+  for (var i = 0; i < artist.length; i++) {
+    // Create query URL for Deezer API call
+    // Make Deezer API call; await until API call is done and store response
+    var deezerResponse = await apiCall(
+      createDeezerUrl(urlIndex * 25, artist[i])
+    );
 
-		// Loop through API response array and add applicable song data to new songs array
-		for (var j = 0; j < deezerResponse.data.length; j++) {
-			var e = deezerResponse.data[j];
-			songs.push({
-				title: e.title,
-				artist: e.artist.name,
-				duration: e.duration,
-			});
-		}
-	}
-	urlIndex++;
-	songs.sort(() => 0.5 - Math.random());
-	console.log("songs");
-	return songs;
+    // Loop through API response array and add applicable song data to new songs array
+    for (var j = 0; j < deezerResponse.data.length; j++) {
+      var e = deezerResponse.data[j];
+      songs.push({
+        title: e.title,
+        artist: e.artist.name,
+        duration: e.duration,
+      });
+    }
+  }
+  urlIndex++;
+  songs.sort(() => 0.5 - Math.random());
+  console.log("songs");
+  return songs;
 }
 
 async function createPlaylist(tripDuration, playlistDuration) {
-	var songs = await getSongs();
-	if (songs.length < 1) {
-		console.log("not enough songs available");
-		return playlistDuration;
-	}
-	for (let i = 0; i < songs.length && playlistDuration < tripDuration; i++) {
-		if (!playlist.find((e) => e.title === songs[i].title)) {
-			playlist.push(songs[i]);
-			playlistDuration += songs[i].duration;
-		}
-	}
-	if (playlistDuration < tripDuration) {
-		return createPlaylist(tripDuration, playlistDuration);
-	}
+  var songs = await getSongs();
+  if (songs.length < 1) {
+    console.log("not enough songs available");
+    return playlistDuration;
+  }
 
-	return playlistDuration;
+  for (let i = 0; i < songs.length && playlistDuration < tripDuration; i++) {
+    if (!playlist.find((e) => e.title === songs[i].title)) {
+      playlist.push(songs[i]);
+      playlistDuration += songs[i].duration;
+    }
+  }
+  if (playlistDuration < tripDuration) {
+    return createPlaylist(tripDuration, playlistDuration);
+  }
+
+  return playlistDuration;
 }
 function createDeezerUrl(index, artist) {
-	return `https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}&index=${index}`;
+  return `https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}&index=${index}`;
 }
 // API call function
 function apiCall(queryURL) {
-	return new Promise((resolve, reject) => {
-		const xhr = new XMLHttpRequest();
-		xhr.open("GET", queryURL);
-		if (queryURL.includes("deezer")) {
-			xhr.setRequestHeader(
-				"x-rapidapi-host",
-				"deezerdevs-deezer.p.rapidapi.com"
-			);
-			xhr.setRequestHeader(
-				"x-rapidapi-key",
-				"ec3c5ea0ebmsh0160b4b2056a0a1p1c3058jsn53757ea9b4b5"
-			);
-		}
-		xhr.onload = function () {
-			if (xhr.status === 200) {
-				resolve(JSON.parse(xhr.responseText));
-			} else {
-				reject("Request failed.  Returned status of " + xhr.status);
-			}
-		};
-		xhr.send();
-	});
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", queryURL);
+    if (queryURL.includes("deezer")) {
+      xhr.setRequestHeader(
+        "x-rapidapi-host",
+        "deezerdevs-deezer.p.rapidapi.com"
+      );
+      xhr.setRequestHeader(
+        "x-rapidapi-key",
+        "ec3c5ea0ebmsh0160b4b2056a0a1p1c3058jsn53757ea9b4b5"
+      );
+    }
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        reject("Request failed.  Returned status of " + xhr.status);
+      }
+    };
+    xhr.send();
+  });
 }
